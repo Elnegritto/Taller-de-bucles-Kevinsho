24b8975b63ee6fe98e46de2fd09aba75d2e3403e
KevinHernandez <129073524+Elnegritto@users.noreply.github.com>
2023-06-19 16:52:39 -0500

    Correccion

--- Bucle For/BucleFor-ejercicio2.py
+++ Bucle For/BucleFor-ejercicio2.py
@@ -1,11 +1,14 @@
 # 2.  Digite un número, si el numero supera a 10, multiplique los 10 primeros 
 
 numero = int(input("Ingrese un número: "))
+resultado = 1
 
 if numero > 10:
-     resultado = 1
-     for i in range(1, 11):
+    for i in range(1, 11):
         resultado *= i
-    
-     print(f"El resultado de multiplicar los 10 primeros números es: {resultado}")
+else:
+    for i in range(1, numero + 1):
+        resultado += i
+
+print(f"El resultado es: {resultado}")
 
